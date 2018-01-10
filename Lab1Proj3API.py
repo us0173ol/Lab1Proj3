1d882e8a4a588adb3c5db129792b2ff7689b2c36
miked <whatthafunk3@aol.com>
2018-01-10 14:28:46 -0600

    Almost finished, couple TODOs left but it works

--- Lab1Proj3API.py
+++ Lab1Proj3API.py
@@ -1,23 +1,24 @@
 #api
 import requests
-import os
 import json
-
-#set ZIP_KEY=6L5Dv9LWOV7VrqVeKk8H3boogJ7ieLFA6av8zk877PzNSA8a3ddm4jtRZtGhctit
-
-#key = os.environ['ZIP_KEY']
-base_url = 'https://www.zipcodeapi.com/rest/6L5Dv9LWOV7VrqVeKk8H3boogJ7ieLFA6av8zk877PzNSA8a3ddm4jtRZtGhctit/distance.json/'
-zip_code1 = input("Please enter the first zip code.")
-zip_code2 = input("Please enter the second zipcode to determine distance between the two.")
-units = 'mile'
-params = {'zip_code1' : zip_code1, 'zip_code2' : zip_code2, 'units' : units}
-#units = input("miles or km")
-#    if units == miles:
-#        units = mile
-#    if units == km:
-#        units = km
-#data = requests.get('https://www.zipcodeapi.com/rest/6L5Dv9LWOV7VrqVeKk8H3boogJ7ieLFA6av8zk877PzNSA8a3ddm4jtRZtGhctit/distance.json/',zip_code1,'/',zip_code2,'/mile')
-data = requests.get(base_url, params)
-
-
+#API key from website for requests
+zip_key='wkJcjnVyfSih8YNlKFJZsUtzLuFeTlFUG7FkotiE5fZG5aW6NjICvL8Vtv6SHNTr'
+#url to send requests to
+base_url = 'https://www.zipcodeapi.com/rest/'
+#Get input from the user for checking the distance between the zip codes
+zip_code1 = input("Please enter the first zip code. ")
+zip_code2 = input("Please enter the second zipcode to determine distance between the two. ")
+#Figure out the distance in either miles or km
+# TODO: error handling for input
+units = input("miles or km?")
+if units == "miles":
+    units = 'mile'
+if units == "km":
+    units = 'km'
+#create the full url to send the request
+new_url = base_url+zip_key+'/distance.json/'+zip_code1+'/'+zip_code2+'/'+units
+data = requests.get(new_url).json()
 print(data)
+#TODO: finish up pulling info from the dictionary and displaying
+#that info properly
+print('-------------------------------------------')
